d127212e78a3b17439e5fc6f4fd456bc72932948
shurintou <shurintou@gmail.com>
2023-07-22 02:46:51 +0900

    style: to show japanese holidays with a dot when at a short width device.

--- src/components/DateButton.vue
+++ src/components/DateButton.vue
@@ -2,8 +2,17 @@
     <div class="ant-picker-cell-inner ant-picker-calendar-date" @click="changeDate(props.date)">
         <div class="ant-picker-calendar-date-value" style="font-size: large; font-weight: bold;"
             :style="{ 'color': dateColor }">
-            {{ props.date.date() }}
-            <span v-if="isJapaneseHoliday">{{ japaneseHolidayName }}</span>
+            <a-row>
+                <a-col :xs="{ span: 14 }" :sm="{ span: 8 }" :md="{ span: 4 }">
+                    {{ props.date.date() }}
+                </a-col>
+                <a-col :xs="{ span: 0 }" :md="{ span: 20 }" v-if="isJapaneseHoliday" class="japanese-holiday-name-text">
+                    <span>{{ japaneseHolidayName }}</span>
+                </a-col>
+                <a-col :xs="{ span: 10 }" :sm="{ span: 16 }" :md="{ span: 0 }" v-if="isJapaneseHoliday">
+                    <span>●</span><!-- to show a dot when at a short width device -->
+                </a-col>
+            </a-row>
         </div>
         <div class="ant-picker-calendar-date-content"></div>
     </div>
@@ -48,5 +57,9 @@ const changeDate = (newDate: Dayjs) => {
 
 .ant-picker-cell-selected .ant-picker-calendar-date .ant-picker-calendar-date-value {
     color: #52c41a !important;
+}
+
+.japanese-holiday-name-text {
+    text-align: left;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { Calendar } from 'ant-design-vue'
-import { DatePicker } from 'ant-design-vue'
-import { Space } from 'ant-design-vue'
-import { Button } from 'ant-design-vue'
+import { Calendar, DatePicker, Space, Button, Row, Col } from 'ant-design-vue'
 import App from './App.vue'
 
 const app = createApp(App)
@@ -13,4 +10,6 @@ app.use(Calendar)
 app.use(DatePicker)
 app.use(Space)
 app.use(Button)
+app.use(Row)
+app.use(Col)
 app.mount('#app')
